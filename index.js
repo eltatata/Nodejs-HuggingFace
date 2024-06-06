6240633f5eef41eb6c90ef0a8475453d1f6456e0
eltatataa <davidtowo75@gmail.com>
2024-06-06 15:43:20 -0500

    translation

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ import { HfInference } from "@huggingface/inference";
 const hf = new HfInference(process.env.HUGGINGFACE_TOKEN)
 
 // -- image to text --
-const model = 'Salesforce/blip-image-captioning-large';
-const imageUrl = "https://i.blogs.es/662997/agujero-blanco/1366_2000.jpg";
+const imageUrl = "https://miro.medium.com/v2/resize:fit:1400/1*PjmGdECv7LAlQW6hm81K6w.png";
 
 try {
     const response = await fetch(imageUrl);
@@ -13,10 +12,26 @@ try {
 
     const text = await hf.imageToText({
         data: blob,
-        model
+        model: 'Salesforce/blip-image-captioning-large'
     });
 
     console.log(text);
+} catch (error) {
+    console.log(error);
+}
+
+// -- translation --
+try {
+    const translation = await hf.translation({
+        model: 'facebook/mbart-large-50-many-to-many-mmt',
+        inputs: "This is a test text to be translated by the model",
+        parameters: {
+            "src_lang": "en_XX",
+            "tgt_lang": "de_DE"
+        }
+    })
+
+    console.log(translation)
 } catch (error) {
     console.log(error);
 }
